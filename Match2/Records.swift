0deb35f6be7c6208217f4b98a85681b4aed02f20
Adilet <bbddbbd2003@gmail.com>
2025-05-02 00:42:21 +0500

    With Records

---
+++ Match2/Records.swift
@@ -0,0 +1,13 @@
+//
+//  Records.swift
+//  Match2
+//
+//  Created by Adilet Kenesbekov on 01.05.2025.
+//
+
+import Foundation
+
+struct Records : Codable {
+  var moves : String
+  var time : String
+}
--- Match2/ViewController.swift
+++ Match2/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
   var moveCount = 0
   var secondsCount = 0
+  var record : Records?
 
   var timer : Timer?
   var isTimeStarted = false
@@ -17,6 +18,12 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var timeLabel: UILabel!
 
+  @IBOutlet weak var recordLabel: UILabel!
+
+  @IBOutlet weak var moveRecord: UILabel!
+  
+  @IBOutlet weak var timeRecord: UILabel!
+  
 
   var images = ["1","2","3","4","5","6","7","8","1","2","3","4","5","6","7","8"].shuffled()
   var cell = [Int](repeating: 0, count: 16)
@@ -24,9 +31,14 @@ class ViewController: UIViewController {
 
 
   override func viewDidLoad() {
+//    UserDefaults.standard.removeObject(forKey: "records")
     super.viewDidLoad()
+    loadRecord()
+    if let record = record{
+      timeRecord.text = record.time
+      moveRecord.text = record.moves
+    }
   }
-
 
   @IBAction func toched(_ sender: UIButton) {
     print(images)
@@ -40,7 +52,7 @@ class ViewController: UIViewController {
     sender.backgroundColor = .white
     cell[sender.tag - 1] = 1
     moveCount += 1
-    movesLabel.text = "Moves : " + String(moveCount)
+    movesLabel.text = String(moveCount)
 
     for (index, value) in cell.enumerated() {
       if value == 1 {
@@ -89,13 +101,15 @@ extension ViewController {
       let alert = UIAlertController(title: "You Won!", message: "To complete the game you needed \(moveCount) moves, and \(String((timeLabel.text!))) time!", preferredStyle: .alert)
       alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { UIAlertAction in
         self.restart()
-        self.movesLabel.text = "Moves : 0"
-        self.timeLabel.text = "00:00:00"
+        self.timeRecord.text = self.record?.time
+        self.moveRecord.text = self.record?.moves
       }))
       present(alert,animated: true)
-      restart()
+      
+//      restart()
 
     }
+
   }
   func startTimer() {
     if !isTimeStarted {
@@ -116,17 +130,79 @@ extension ViewController {
   }
 
   func restart() {
+    saveRecords()
     images = ["1","2","3","4","5","6","7","8","1","2","3","4","5","6","7","8"].shuffled()
     for i in 0...15 {
       cell[i] = 0
       let button = view.viewWithTag(i+1) as! UIButton
       button.setBackgroundImage(nil, for: .normal)
       button.backgroundColor = .systemTeal
-      moveCount = 0
-      secondsCount = 0
-      timer = nil
-      isTimeStarted = false
+
     }
+    moveCount = 0
+    secondsCount = 0
+    isTimeStarted = false
+    timer?.invalidate()
+    timer = nil
+  }
+
+
+
+  func saveRecords () {
+    guard let move = movesLabel.text, let time = timeLabel.text else { return }
+
+    let updatedRecord = Records(moves: move, time: time)
+
+    if record == nil {
+      do{
+        let encodedData = try JSONEncoder().encode([updatedRecord])
+        UserDefaults.standard.set(encodedData, forKey: "records")
+      } catch{
+        print("Unable to save record: \(error)")
+      }
+      record = updatedRecord
+      return
+    }
+
+    do {
+      if let data = UserDefaults.standard.data(forKey: "records") {
+        var array = try JSONDecoder().decode([Records].self, from: data)
+        let oldTime = convertToSeconds(record!.time)
+        let newTime = convertToSeconds(updatedRecord.time)
+        if newTime < oldTime || (oldTime == newTime && Int(updatedRecord.moves)! < Int(record!.moves)!) {
+          array.removeAll()
+          array.append(updatedRecord)
+          let encodedData = try JSONEncoder().encode(array)
+          UserDefaults.standard.set(encodedData, forKey: "records")
+          record = updatedRecord
+        }
+      }
+    } catch {
+      print("Unable to save a record \(error)")
+    }
+
+  }
+
+  func convertToSeconds(_ time : String) -> Int{
+    let hh = Int(time.split(separator: ":")[0]) ?? 0
+    let mm = Int(time.split(separator: ":")[1]) ?? 0
+    let ss = Int(time.split(separator: ":")[2]) ?? 0
+    return hh * 3600 + mm * 60 + ss
+  }
+
+
+
+  func loadRecord() {
+      if let data = UserDefaults.standard.data(forKey: "records") {
+          do {
+              let array = try JSONDecoder().decode([Records].self, from: data)
+              if let savedRecord = array.first {
+                  record = savedRecord
+              }
+          } catch {
+              print("Unable to load record: \(error)")
+          }
+      }
   }
 
 }
